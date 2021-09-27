579bf77a20cf2f4853c97c842808ab569886b4fd
samu.musta <samu.musta@myy.haaga-helia.fi>
2021-09-27 15:59:52 +0300

    Create AbcRandomizer() 3 letter sequencer for player UI

--- HH_Braingame/Assets/Scripts/ReactionProblemCheck.cs
+++ HH_Braingame/Assets/Scripts/ReactionProblemCheck.cs
@@ -6,14 +6,13 @@ public class ReactionProblemCheck : MonoBehaviour
 {
     PlayerMovement move;
     DisplayChallenge display;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Init jump,  method
         move = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
-        
-        display = GameObject.FindGameObjectWithTag("Player").
-        GetComponent<DisplayChallenge>();
-
+        display = GameObject.FindGameObjectWithTag("Player").GetComponent<DisplayChallenge>();
     }
 
     // Update is called once per frame
@@ -21,12 +20,31 @@ public class ReactionProblemCheck : MonoBehaviour
     {
 
     }
-    
 
+    string[] AbcRandomizer()
+    {
+        // Lenght 26
+        string[] abcArray = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
-    // MyScript script = obj.AddComponent<MyScript>();
+        //Initialize blank list, array
+        List<string> draw = new List<string>();
+        string[] result;
 
-    // checks if the player hit the collider
+        int seed = Random.Range(0, 23);
+        int end = seed + 2;
+
+        while (seed <= end)
+        {
+            draw.Add(abcArray[seed]);
+            seed++;
+        }
+
+        result = draw.ToArray();
+
+        return result;
+    }
+
+    //Check if the player hit the collider
     void OnTriggerEnter2D(Collider2D other)
     {
         
@@ -39,29 +57,21 @@ public class ReactionProblemCheck : MonoBehaviour
 
     IEnumerator CheckPress()
     {
+        string[] ui_values = AbcRandomizer();
+
         while (true)
         {
+            //Init timer, success condition
             float timer = 1.75f;
             bool success = false;
+
+            //Check 1st trial
             while (success == false && timer > 0f)
             {
                 timer -= Time.deltaTime; // reduce timer 
-                display.DisplayButton("R");
+                display.DisplayButton(ui_values[0]);
                 success = Input.GetKeyDown(KeyCode.R);
-                yield return null;
-            }
-            if (success == false)
-            {
-                Debug.Log("Lost");
-                yield break;
-            }
-            success = false;
-            timer = 1.75f;
-            while (success == false && timer > 0f)
-            {
-                timer -= Time.deltaTime; // reduce timer 
-                success = Input.GetKeyDown(KeyCode.V);
-                display.DisplayButton("V");
+                Debug.Log("KeyCode ToString: " + KeyCode.R.ToString());
                 yield return null;
             }
             if (success == false)
@@ -70,6 +80,25 @@ public class ReactionProblemCheck : MonoBehaviour
                 yield break;
             }
 
+            //Reset timer, success condition
+            success = false;
+            timer = 1.75f;
+
+            //Check 2nd trial
+            while (success == false && timer > 0f)
+            {
+                timer -= Time.deltaTime; // reduce timer 
+                success = Input.GetKeyDown(KeyCode.V);
+                display.DisplayButton(ui_values[2]);
+                yield return null;
+            }
+            if (success == false)
+            {
+                Debug.Log("Lost");
+                yield break;
+            }
+
+            //Else success
             Debug.Log("Won");
             move.Jump();
             display.DisplayButton("");
