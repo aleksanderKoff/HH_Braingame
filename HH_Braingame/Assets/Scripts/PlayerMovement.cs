99c59c66d9f416f0703166fd2f985072b196412b
nicodeemus <bgj359@myy.haaga-helia.fi>
2021-09-27 14:43:29 +0300

    basic rough code for rotation character

--- HH_Braingame/Assets/Scripts/PlayerMovement.cs
+++ HH_Braingame/Assets/Scripts/PlayerMovement.cs
@@ -12,8 +12,9 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] public float moveSpeed = 7f;
     [SerializeField] public float jumpForce = 14f;
+    float angle;
 
-   
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -31,6 +32,9 @@ public class PlayerMovement : MonoBehaviour
         {
             Jump();
         }
+
+        TurnCharacter();
+       
     }
     public void Jump()
     {
@@ -40,6 +44,28 @@ public class PlayerMovement : MonoBehaviour
     public bool IsGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
+    public void TurnCharacter()
+    {
+        int layerMask = 1 << 8; //määrittää mikä layer otetaan mukaan layermaskiin joka annetaan Raycastille, tässä tapauksessa 'Platform' rayvast katsoo ainoastaan tätä layeria sitten
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0.2f, 0, 0), -transform.up, Mathf.Infinity, layerMask);
+
+        angle = Vector3.Angle(hit.normal, new Vector3(0, 1, 0));
+
+        if (hit.normal.x > 0)
+        {
+            angle = -angle;
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, angle); 
+
+        //Vector3 forward = transform.TransformDirection(Vector3.forward) * 20;
+        //Debug.DrawRay(transform.position + new Vector3(0.7f, 0, 0), -transform.up, Color.red); 
+
+        //Näillä saa näkyvän DrawRay:n jos haluaa alkaa vielä jotain debuggailemaan
+
+
     }
 
 }
