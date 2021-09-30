90ff60d480a78552770479b44956e27b8bf23e1f
bgg274 <bgg274@myy.haaga-helia.fi>
2021-09-30 15:40:41 +0300

    Add show/hide buttons

--- HH_Braingame/Assets/Scripts/DisplayChallenge.cs
+++ HH_Braingame/Assets/Scripts/DisplayChallenge.cs
@@ -16,4 +16,9 @@ public class DisplayChallenge : MonoBehaviour
     {
         challenge.text = character;
     }
+
+    public void Clear()
+    {
+        challenge.text = "";
+    }
 }
--- HH_Braingame/Assets/Scripts/ReactionProblemCheck.cs
+++ HH_Braingame/Assets/Scripts/ReactionProblemCheck.cs
@@ -6,13 +6,23 @@ public class ReactionProblemCheck : MonoBehaviour
 {
     PlayerMovement move;
     DisplayChallenge display;
+    UnityEngine.UI.Button button1;
+    UnityEngine.UI.Button button2;
+    UnityEngine.UI.Button button3;
 
     // Start is called before the first frame update
     void Start()
     {
         // Init jump,  method
         move = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+
         display = GameObject.FindGameObjectWithTag("Player").GetComponent<DisplayChallenge>();
+        //Init buttons
+        button1 = GameObject.Find("Button").GetComponent<UnityEngine.UI.Button>();
+        button2 = GameObject.Find("Button (1)").GetComponent<UnityEngine.UI.Button>();
+        button3 = GameObject.Find("Button (2)").GetComponent<UnityEngine.UI.Button>();
+        //Set buttons invisible
+        ButtonSetFalse(button1, button2, button3);
     }
 
     // Update is called once per frame
@@ -50,6 +60,20 @@ public class ReactionProblemCheck : MonoBehaviour
         KeyCode keyCode = (KeyCode) System.Enum.Parse(typeof(KeyCode), key);
         return keyCode;
     }
+    // Hide Button objects(1, 2, 3)
+    void ButtonSetFalse(UnityEngine.UI.Button a, UnityEngine.UI.Button b, UnityEngine.UI.Button c)
+    {
+        button1.gameObject.SetActive(false);
+        button2.gameObject.SetActive(false);
+        button3.gameObject.SetActive(false);
+    }
+    // Show Button objects(1, 2, 3)
+    void ButtonSetTrue(UnityEngine.UI.Button a, UnityEngine.UI.Button b, UnityEngine.UI.Button c)
+    {
+        button1.gameObject.SetActive(true);
+        button2.gameObject.SetActive(true);
+        button3.gameObject.SetActive(true);
+    }
 
     //Check if the player hit the collider
     void OnTriggerEnter2D(Collider2D other)
@@ -77,20 +101,25 @@ public class ReactionProblemCheck : MonoBehaviour
             while (success == false && timer > 0f)
             {
                 timer -= Time.deltaTime; // reduce timer 
-                display.TwoCharacters(ui_values[0], "    _    ", ui_values[2]);
+                display.TwoCharacters(ui_values[0], "    ?    ", ui_values[2]);
+                //Set buttons visible
+                ButtonSetTrue(button1, button2, button3);
                 success = Input.GetKeyDown(Convert(ui_values[1]));
                 yield return null;
             }
             if (success == false)
             {
                 Debug.Log("Lost");
+                ButtonSetFalse(button1, button2, button3);
+                display.Clear();
                 yield break;
             }
 
             //Else success
             Debug.Log("Won");
             move.Jump();
-            display.OneCharacter("");
+            display.Clear();
+            ButtonSetFalse(button1, button2, button3);
             break;
         }
     }
