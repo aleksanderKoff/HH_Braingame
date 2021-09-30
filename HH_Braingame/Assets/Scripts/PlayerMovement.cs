14ed5101edce10e8920f892cb86d029fcbf293f0
samu.musta <samu.musta@myy.haaga-helia.fi>
2021-09-30 14:39:48 +0300

    Revert "merge character_rotation with develop"
    This reverts commit 9292e0280b58a44979c9f21407742ebe1d8f1094, reversing
    changes made to o3117993ccc53aa54c6881d2a420d602bd4bb6d4.
    
    Merge cause confligts with identifiers

--- HH_Braingame/Assets/Scripts/PlayerMovement.cs
+++ HH_Braingame/Assets/Scripts/PlayerMovement.cs
@@ -6,22 +6,19 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
     public BoxCollider2D coll;
-    float angle = 0;
-
+    
     [SerializeField] public LayerMask jumpableGround;
 
 
     [SerializeField] public float moveSpeed = 7f;
     [SerializeField] public float jumpForce = 14f;
-  
 
-
+   
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
-        
     }
 
     // Update is called once per frame
@@ -34,9 +31,6 @@ public class PlayerMovement : MonoBehaviour
         {
             Jump();
         }
-
-        TurnCharacter();
-       
     }
     public void Jump()
     {
@@ -46,30 +40,6 @@ public class PlayerMovement : MonoBehaviour
     public bool IsGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
-    }
-
-    public void TurnCharacter()
-    {
-        
-        int layerMask = 1 << 8; //määrittää mikä layer otetaan mukaan layermaskiin joka annetaan Raycastille, tässä tapauksessa 'Platform'. raycast katsoo ainoastaan tätä layeria sitten
-        
-        RaycastHit2D frontSensor = Physics2D.Raycast(transform.position + new Vector3(0.7f, 0, 0), -transform.up, 2, layerMask);
-        RaycastHit2D backSensor = Physics2D.Raycast(transform.position + new Vector3(0.1f, 0, 0), -transform.up, 2, layerMask);
-
-
-        if (frontSensor.normal.x > 0 && backSensor.normal.x > 0 && Mathf.Abs(frontSensor.normal.x - backSensor.normal.x) < 0.05)
-            angle = -Vector3.Angle(frontSensor.normal, new Vector3(0, 1, 0));
-        else if (frontSensor.normal.x < 0 && backSensor.normal.x < 0 && Mathf.Abs(frontSensor.normal.x - backSensor.normal.x) < 0.05)
-            angle = Vector3.Angle(frontSensor.normal, new Vector3(0, 1, 0));
-
-        if (angle<=45 && angle>=-45)
-        transform.rotation = Quaternion.Euler(0, 0, angle); 
-
-        //Vector3 forward = transform.TransformDirection(Vector3.forward) * 20;
-        //Debug.DrawRay(transform.position + new Vector3(0.7f, 0, 0), -transform.up, Color.red);
-        //Näillä saa näkyvän DrawRay:n jos haluaa alkaa vielä jotain debuggailemaan
-
-
     }
 
 }
