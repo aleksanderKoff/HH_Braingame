38f2c546a94bad575297aa72c6755055ebeeee3c
nicodeemus <bgj359@myy.haaga-helia.fi>
2021-09-30 15:49:27 +0300

    add rotation code

--- HH_Braingame/Assets/Scripts/PlayerMovement.cs
+++ HH_Braingame/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
     public BoxCollider2D coll;
-    
+    float angle = 0;
+
     [SerializeField] public LayerMask jumpableGround;
 
 
@@ -31,6 +32,8 @@ public class PlayerMovement : MonoBehaviour
         {
             Jump();
         }
+
+        TurnCharacter();
     }
     public void Jump()
     {
@@ -41,5 +44,29 @@ public class PlayerMovement : MonoBehaviour
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
+
+    public void TurnCharacter()
+    {
+
+        int layerMask = 1 << 8; //määrittää mikä layer otetaan mukaan layermaskiin joka annetaan Raycastille, tässä tapauksessa 'Platform'. raycast katsoo ainoastaan tätä layeria sitten
+
+        RaycastHit2D frontSensor = Physics2D.Raycast(transform.position + new Vector3(0.7f, 0, 0), -transform.up, 2, layerMask);
+        RaycastHit2D backSensor = Physics2D.Raycast(transform.position + new Vector3(0.1f, 0, 0), -transform.up, 2, layerMask);
+
+
+        if (frontSensor.normal.x > 0 && backSensor.normal.x > 0 && Mathf.Abs(frontSensor.normal.x - backSensor.normal.x) < 0.05)
+            angle = -Vector3.Angle(frontSensor.normal, new Vector3(0, 1, 0));
+        else if (frontSensor.normal.x < 0 && backSensor.normal.x < 0 && Mathf.Abs(frontSensor.normal.x - backSensor.normal.x) < 0.05)
+            angle = Vector3.Angle(frontSensor.normal, new Vector3(0, 1, 0));
+
+        if (angle <= 45 && angle >= -45)
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        //Vector3 forward = transform.TransformDirection(Vector3.forward) * 20;
+        //Debug.DrawRay(transform.position + new Vector3(0.7f, 0, 0), -transform.up, Color.red);
+
+        //Näillä saa näkyvän DrawRay:n jos haluaa alkaa vielä jotain debuggailemaan
+    }
+
 
 }
