0fe2433ebf7c6a6f7d9abae340012f99b0ee98c3
aleksanderKoff <aleksander.korolkoff@gmail.com>
2021-10-11 14:13:26 +0300

    Add clicker script

---
+++ HH_Braingame/Assets/Scripts/Clicker.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    [SerializeField] public LayerMask Clickable;
+    private void Update()
+    {
+
+        int layerMask = 1 << 10;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero, layerMask);
+            if (hit.collider != null)
+            {
+                PrintName(hit.collider.gameObject);
+            }
+        }
+    }
+
+    private void PrintName(GameObject go)
+    {
+        Debug.Log(go.name);
+    }
+}
