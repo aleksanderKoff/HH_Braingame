33562bd9e89aae156eb1f0e68ea6e672d05d8e86
aleksanderKoff <aleksander.korolkoff@gmail.com>
2021-10-28 12:28:19 +0300

    Add re-balanced reaction check

--- HH_Braingame/Assets/Scripts/ReactionProblemV2.cs
+++ HH_Braingame/Assets/Scripts/ReactionProblemV2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReactionProblemCheck : MonoBehaviour
+public class ReactionProblemV2 : MonoBehaviour
 {
     PlayerMovement move;
     DisplayChallenge display;
@@ -16,15 +16,15 @@ public class ReactionProblemCheck : MonoBehaviour
     void Start()
     {
         // Init jump,  method
-        // move = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        move = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
 
-        // display = GameObject.FindGameObjectWithTag("Player").GetComponent<DisplayChallenge>();
+        display = GameObject.FindGameObjectWithTag("Player").GetComponent<DisplayChallenge>();
         //Init buttons
-        // button1 = GameObject.Find("Button").GetComponent<UnityEngine.UI.Button>();
-        // button2 = GameObject.Find("Button (1)").GetComponent<UnityEngine.UI.Button>();
-        // button3 = GameObject.Find("Button (2)").GetComponent<UnityEngine.UI.Button>();
+        button1 = GameObject.Find("Button").GetComponent<UnityEngine.UI.Button>();
+        button2 = GameObject.Find("Button (1)").GetComponent<UnityEngine.UI.Button>();
+        button3 = GameObject.Find("Button (2)").GetComponent<UnityEngine.UI.Button>();
         //Set buttons invisible
-        // ButtonSetFalse(button1, button2, button3);
+        ButtonSetFalse(button1, button2, button3);
     }
 
     // Update is called once per frame
@@ -36,7 +36,7 @@ public class ReactionProblemCheck : MonoBehaviour
     string[] AbcRandomizer()
     {
         // Lenght 26
-        string[] abcArray = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        string[] abcArray = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
 
         //Initialize blank list, array
         List<string> draw = new List<string>();
@@ -59,7 +59,7 @@ public class ReactionProblemCheck : MonoBehaviour
     //Convert string to KeyCode
     KeyCode Convert(string key)
     {
-        KeyCode keyCode = (KeyCode) System.Enum.Parse(typeof(KeyCode), key);
+        KeyCode keyCode = (KeyCode)System.Enum.Parse(typeof(KeyCode), key);
         return keyCode;
     }
     // Hide Button objects(1, 2, 3)
@@ -70,30 +70,28 @@ public class ReactionProblemCheck : MonoBehaviour
         button3.gameObject.SetActive(false);
     }
     // Show Button objects(1, 2, 3)
-    void ButtonSetTrue(UnityEngine.UI.Button a, UnityEngine.UI.Button b, UnityEngine.UI.Button c)
+    void ButtonSetTrue(UnityEngine.UI.Button b)
     {
-        button1.gameObject.SetActive(true);
         button2.gameObject.SetActive(true);
-        button3.gameObject.SetActive(true);
     }
 
     //Check if the player hit the collider
     void OnTriggerEnter2D(Collider2D other)
     {
-        
-        if (other.tag == "Player" && gameObject.tag == "Trigger")
+
+        if (other.tag == "Player" && gameObject.tag == "Trigger1")
         {
             Debug.Log("COLLIDED");
-            StartCoroutine(CheckPress(timer));
+            StartCoroutine(CheckPress1(timer));
         }
     }
 
-    IEnumerator CheckPress(float timer)
+    IEnumerator CheckPress1(float timer)
     {
         // REFAKTOROI
         string[] ui_values = AbcRandomizer();
-        Debug.Log("ui_values:" + ui_values[0] + "(" + ui_values[1] + ")" + ui_values[2]);
-        
+        Debug.Log("ui_values:" + "(" + ui_values[1] + ")");
+
         while (true)
         {
             //Init success condition
@@ -103,9 +101,9 @@ public class ReactionProblemCheck : MonoBehaviour
             while (success == false && timer > 0f)
             {
                 timer -= Time.deltaTime; // reduce timer 
-                display.TwoCharacters(ui_values[0], "        _        ", ui_values[2]); // 2 tabs + 2-2 spaces
+                display.OneCharacter(ui_values[1]); // 2 tabs + 2-2 spaces
                 //Set buttons visible
-                ButtonSetTrue(button1, button2, button3);
+                ButtonSetTrue(button2);
                 success = Input.GetKeyDown(Convert(ui_values[1]));
                 yield return null;
             }
