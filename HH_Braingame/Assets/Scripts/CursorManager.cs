3c6545b31a34e0f0c0f3cf3dd6095edfb3a3a291
nicodeemus <bgj359@myy.haaga-helia.fi>
2021-11-11 11:38:45 +0200

    change cursor path

--- HH_Braingame/Assets/Scripts/CursorManager.cs
+++ HH_Braingame/Assets/Scripts/CursorManager.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class CursorManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField] private Texture2D CursorDefaultReference;
-    [SerializeField] private Texture2D CursorHitReference;
-
+    
     public static Texture2D CursorTextureDefault;
     public static Texture2D CursorTextureHit;
     public static Vector2 CursorHotspot;
 
     void Start()
     {
-        CursorTextureDefault = CursorDefaultReference;
-        CursorTextureHit = CursorHitReference;
+        CursorTextureDefault = Resources.Load<Texture2D>("Cursor/Cursor");
+        CursorTextureHit = Resources.Load<Texture2D>("Cursor/CursorRed");
         CursorHotspot = new Vector2(CursorTextureDefault.width / 2, CursorTextureDefault.height / 2);
         Cursor.SetCursor(CursorTextureDefault, CursorHotspot, CursorMode.Auto);
     }
