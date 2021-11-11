5dec9f35cae2c3829799d789b7be297eeb465484
bgg274 <bgg274@myy.haaga-helia.fi>
2021-11-11 14:51:50 +0200

    Create EsCan grid spawn in bossfight

--- HH_Braingame/Assets/Scripts/Player/StopPlayer.cs
+++ HH_Braingame/Assets/Scripts/Player/StopPlayer.cs
@@ -6,10 +6,14 @@ public class StopPlayer : MonoBehaviour
 {
     public GameObject player;
     public GameObject projectile;
+    BossGrid boss_grid;
 
     void Start()
     {
         projectile.GetComponent<DestroyProjectile>().enabled = false;
+        boss_grid = GameObject.Find("BossGrid").GetComponent<BossGrid>();
+        boss_grid.enabled = false;
+        Debug.Log(boss_grid.enabled == true);
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -17,6 +21,7 @@ public class StopPlayer : MonoBehaviour
         if (other.tag == "FreezePlayer") //stop player movement on trigger
         {
             Debug.Log("Freeze player");
+            boss_grid.enabled = true;
             player.GetComponent<PlayerMovement> ().enabled = false; //disable PlayerMovement script
             if (projectile != null)
             {
