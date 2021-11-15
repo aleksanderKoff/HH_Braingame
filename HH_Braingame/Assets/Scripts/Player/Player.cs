641bf7caea90e930cd26fe718b447fa1e128f7cf
aleksanderKoff <aleksander.korolkoff@gmail.com>
2021-11-15 12:04:07 +0200

    Add separate respawn script to handle player respawn

--- HH_Braingame/Assets/Scripts/Player/Player.cs
+++ HH_Braingame/Assets/Scripts/Player/Player.cs
@@ -4,46 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Transform player;
-    [SerializeField] private Transform respawnPoint;
-    [SerializeField] private Transform checkPoint;
     BossHealth boss;
-    public int fallBoundary = -20;
     private Camera cam;
+    [SerializeField] public Transform playerPosition;
+    [SerializeField] public Transform respawnPoint;
+    public static Transform respawnLocation;
+    public static Transform playerLocation;
 
     void Start()
     {
+        playerLocation = playerPosition;
+        respawnLocation = respawnPoint;
         cam = Camera.main;
         boss = GameObject.Find("DestroyProjectile").GetComponent<BossHealth>();
     }
-    // Update is called once per frame
-    void Update()
-    {
-        //if player falls below boundary, change player position to respawn position
-        if (transform.position.y <= fallBoundary)
-        {
-            player.transform.position = new Vector3(respawnPoint.transform.position.x, respawnPoint.transform.position.y, player.transform.position.z);
-        }
 
-
-    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         //kill player if hit by projectile
         if (collision.tag == "Projectile" && gameObject && boss.killed == false)
         {
-            player.transform.position = new Vector3(respawnPoint.transform.position.x, respawnPoint.transform.position.y, player.transform.position.z);
+            Respawn.RespawnPlayer();
             GameObject.Find("Camera").GetComponent<cameraController>().enabled = true;
             GameObject.Find("ProjectileSpawner").GetComponent<drop_projectile>().enabled = false;
             gameObject.GetComponent<PlayerMovement> ().enabled = true;
             cam.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 6f, 2f);
-        }
-        //change respawn position to checkpoint when collided
-        if (collision.tag == "Checkpoint")
-        {
-            Debug.Log("Checkpoint reached!");
-            
-            respawnPoint.transform.position = collision.transform.position;
         }
     }
 
